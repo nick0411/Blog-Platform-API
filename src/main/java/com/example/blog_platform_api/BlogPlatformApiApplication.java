b091b030bb264c89b960a0916ed3941efa73c8c0
nick <nlh041120@gmail.com>
2025-06-24 01:25:54 +0630

    Initial commit: blog platform api

---
+++ src/main/java/com/example/blog_platform_api/BlogPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPlatformApiApplication.class, args);
+	}
+
+}
